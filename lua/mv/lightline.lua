f856deacd9a886a07c721cca999ba508b0b458d0
Michal Vanco <michal@maximale.sk>
2021-08-13 11:17:50 +0200

    Lightline

--- lua/mv/lightline.lua
+++ lua/mv/lightline.lua
@@ -20,15 +20,39 @@ function! Lightline_Fileformat()
 endfunction
 ]], false)
 
+vim.api.nvim_exec([[
+function! Lightline_GitBranch()
+    let branchname = FugitiveHead()
+    if strlen(branchname) > 0
+        return ' ' .. branchname
+    endif
+    return ''
+endfunction
+]], false)
+
+vim.api.nvim_exec([[
+function! Lightline_Filename()
+  let filename = expand('%:t') !=# '' ? expand('%:t') : '[No Name]'
+  let modified = &modified ? ' +' : ''
+  return filename . modified
+endfunction
+]], false)
+
 vim.g.lightline = {
     colorscheme = 'Tomorrow_Night_Eighties',
-        active = {
-            left = { {'mode', 'paste'}, {'readonly', 'filename', 'modified' }},
-            right = { {'lineinfo'}, {'percent'}, {'lspclient', 'fileformat', 'fileencoding', 'filetype' }}
-        },
+    active = {
+        left = { { 'mode', 'paste' }, { 'readonly', 'filename' }, { 'gitbranch' } },
+        right = {
+            { 'lineinfo' },
+            { 'percent' },
+            { 'lspclient', 'fileformat', 'fileencoding', 'filetype' }
+        }
+    },
     component_function = {
+        filename = 'Lightline_Filename',
         lspclient = 'Lightline_LspClient',
         filetype = 'Lightline_Filetype',
-        fileformat = 'Lightline_Fileformat'
+        fileformat = 'Lightline_Fileformat',
+        gitbranch = 'Lightline_GitBranch'
     }
 }
