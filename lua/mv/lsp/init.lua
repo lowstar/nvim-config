3f4cda2acfa2b78b586fdc17f5753fd74ad3e396
Michal Vanco <michal@maximale.sk>
2021-08-16 22:57:03 +0200

    Delete unused LSP handler

--- lua/mv/lsp/init.lua
+++ lua/mv/lsp/init.lua
@@ -7,8 +7,6 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
     vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
                  { virtual_text = true, signs = true, update_in_insert = true, underline = true })
 
-vim.lsp.handlers["textDocument/hover"] = require'lspsaga.hover'.handler
-
 vim.fn.sign_define("LspDiagnosticsSignError", {
     texthl = "LspDiagnosticsSignError",
     text = "",
