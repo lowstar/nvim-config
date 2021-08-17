9b17775e259f6fb633ff77e8e707640c42fb5a2a
Michal Vanco <michal@maximale.sk>
2021-08-17 09:03:00 +0200

    telescope-hop, telescope-fzf-native

--- lua/mv/plugins.lua
+++ lua/mv/plugins.lua
@@ -33,8 +33,9 @@ return require('packer').startup(function(use)
     use 'nvim-lua/popup.nvim'
     use 'nvim-lua/plenary.nvim'
     use 'nvim-telescope/telescope.nvim'
-    use 'nvim-telescope/telescope-fzy-native.nvim'
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use 'nvim-telescope/telescope-symbols.nvim'
+    use 'nvim-telescope/telescope-hop.nvim'
 
     use 'neovim/nvim-lspconfig'
     use 'onsails/lspkind-nvim'
--- lua/mv/telescope.lua
+++ lua/mv/telescope.lua
@@ -2,12 +2,22 @@ local actions = require('telescope.actions')
 
 require('telescope').setup {
     defaults = {
-        file_sorter = require('telescope.sorters').get_fzy_sorter,
-
         prompt_prefix = '❯ ',
         selection_caret = '❯ ',
 
-        mappings = { i = { ["<esc>"] = actions.close } },
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close,
+                ["<C-h>"] = require"telescope".extensions.hop.hop,
+                ["<C-space>"] = function(prompt_bufnr)
+                    local opts = {
+                        callback = actions.toggle_selection,
+                        loop_callback = actions.send_selected_to_qflist
+                    }
+                    require("telescope").extensions.hop._hop_loop(prompt_bufnr, opts)
+                end
+            }
+        },
 
         layout_config = {
             horizontal = { width_padding = 0.05, height_padding = 0.1, preview_width = 0.6 },
@@ -17,10 +27,36 @@ require('telescope').setup {
 
         file_ignore_patterns = { "node_modules", "target", ".git", ".vscode" }
     },
-    extensions = { fzy_native = { override_generic_sorter = false, override_file_sorter = true } }
+    extensions = {
+        fzf = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+            case_mode = 'smart_case'
+        },
+        hop = {
+            -- keys define your hop keys in order; defaults to roughly lower- and uppercased home row
+            -- shown keys here are only subset of defaults!
+            keys = { "a", "s", "d", "f", "g", "h", "j", "k", "l", ";" }, -- ... and more
+
+            -- Highlight groups to link to signs and lines; the below configuration refers to demo
+            -- sign_hl typically only defines foreground to possibly be combined with line_hl
+            sign_hl = { "WarningMsg", "Title" },
+            -- optional, typically a table of two highlight groups that are alternated between
+            line_hl = { "CursorLine", "Normal" },
+            -- options specific to `hop_loop`
+            -- true temporarily disables Telescope selection highlighting
+            clear_selection_hl = false,
+            -- highlight hopped to entry with telescope selection highlight
+            -- note: mutually exclusive with `clear_selection_hl`
+            trace_entry = true,
+            -- jump to entry where hoop loop was started from
+            reset_selection = true
+        }
+    }
 }
 
-require('telescope').load_extension('fzy_native')
+require('telescope').load_extension('fzf')
+require('telescope').load_extension('hop')
 
 local M = {}
 
