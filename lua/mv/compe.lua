e13caa6cb96f80c14885cbc678194662ee5204b2
Michal Vanco <michal@maximale.sk>
2021-08-19 09:50:20 +0200

    Cleanup compe configuration

--- lua/mv/compe.lua
+++ lua/mv/compe.lua
@@ -1,68 +1,13 @@
 require'compe'.setup {
-    enabled = true,
-    autocomplete = true,
-    debug = false,
-    min_length = 1,
-    preselect = 'enable',
-    throttle_time = 80,
-    source_timeout = 200,
-    incomplete_delay = 400,
-    max_abbr_width = 100,
-    max_kind_width = 100,
-    max_menu_width = 100,
-
-    documentation = {
-        border = 'rounded';
-        -- border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
-        winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-        max_width = 120,
-        min_width = 60,
-        max_height = math.floor(vim.o.lines * 0.3),
-        min_height = 1,
-    },
-
     source = {
-        path = {kind = "  "},
-        buffer = {kind = "  "},
-        calc = {kind = "  "},
-        vsnip = {kind = "  "},
-        nvim_lsp = {kind = "  "},
-        nvim_lua = {kind = "  "},
-        spell = {kind = "  "},
-        tags = false,
-        snippets_nvim = {kind = "  "},
-        treesitter = {kind = "  "},
-        emoji = false
-        -- emoji = {kind = " ﲃ "}
-        -- for emoji press : (idk if that in compe tho)
+        path = true,
+        buffer = true,
+        calc = true,
+        nvim_lsp = true,
+        nvim_lua = true,
+        treesitter = true
     }
 }
-
--- 
--- 
--- 
--- 
--- 
--- 
--- 
--- 
--- 
--- 
--- 
--- 
--- 
--- 
--- 
--- 
--- ﬘
--- 
--- 
--- 
--- m
--- 
--- 
--- 
--- 
 
 local t = function(str)
     return vim.api.nvim_replace_termcodes(str, true, true, true)
@@ -80,7 +25,7 @@ end
 _G.tab_complete = function()
     if vim.fn.pumvisible() == 1 then
         return t "<C-n>"
-    elseif vim.fn.call("vsnip#available", {1}) == 1 then
+    elseif vim.fn.call("vsnip#available", { 1 }) == 1 then
         return t "<Plug>(vsnip-expand-or-jump)"
     elseif check_back_space() then
         return t "<Tab>"
@@ -92,23 +37,27 @@ end
 _G.s_tab_complete = function()
     if vim.fn.pumvisible() == 1 then
         return t "<C-p>"
-    elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+    elseif vim.fn.call("vsnip#jumpable", { -1 }) == 1 then
         return t "<Plug>(vsnip-jump-prev)"
     else
         return t "<S-Tab>"
     end
 end
 
-vim.api.nvim_set_keymap("i", "<Tab>",   "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>",   "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
 
-
-vim.api.nvim_set_keymap("i", '<C-Space>', "compe#complete()", { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", '<C-Space>', "compe#complete()",
+                        { noremap = true, silent = true, expr = true })
 -- vim.api.nvim_set_keymap("i", '<CR>',      "compe#confirm('<CR>')", { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("i", '<CR>',      [[compe#confirm(luaeval("require 'nvim-autopairs'.autopairs_cr()"))]], { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("i", '<C-e>',     "compe#close('<C-e>')", { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("i", '<C-f>',     "compe#scroll({ 'delta': +4 })", { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("i", '<C-d>',     "compe#scroll({ 'delta': -4 })", { noremap = true, silent = true, expr = true })
-
+vim.api.nvim_set_keymap("i", '<CR>',
+                        [[compe#confirm(luaeval("require 'nvim-autopairs'.autopairs_cr()"))]],
+                        { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", '<C-e>', "compe#close('<C-e>')",
+                        { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", '<C-f>', "compe#scroll({ 'delta': +4 })",
+                        { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", '<C-d>', "compe#scroll({ 'delta': -4 })",
+                        { noremap = true, silent = true, expr = true })
