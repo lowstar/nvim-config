75163a8c0729e7aa516a5dcaff0969e6f1c21a1a
Michal Vanco <michal@maximale.sk>
2021-08-23 10:18:46 +0200

    Enable fuzzy

--- lua/mv/telescope.lua
+++ lua/mv/telescope.lua
@@ -29,6 +29,7 @@ require('telescope').setup {
     },
     extensions = {
         fzf = {
+            fuzzy = true,
             override_generic_sorter = false,
             override_file_sorter = true,
             case_mode = 'smart_case'
