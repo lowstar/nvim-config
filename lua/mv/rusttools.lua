278fd683673e9365ad0847536ba14b8f500a6578
Michal Vanco <michal@maximale.sk>
2021-09-02 14:46:20 +0200

    Change debounce to 150

--- lua/mv/rusttools.lua
+++ lua/mv/rusttools.lua
@@ -1,7 +1,7 @@
 require('rust-tools').setup({
     tools = { inlay_hints = { highlight = "InlayHint" }, hover_actions = { auto_focus = true } },
     server = {
-        flags = { allow_incremental_sync = true, debounce_text_changes = 500 },
+        flags = { allow_incremental_sync = true, debounce_text_changes = 150 },
         on_attach = function(client, bufnr)
             require'mv.lsp'.common_on_attach(client, bufnr)
             vim.api.nvim_buf_set_keymap(bufnr, 'v', 'K', ":<C-U>RustHoverRange<cr>",
