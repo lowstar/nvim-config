d6ede0857ed9d5f96514becf50f928afe739b1d2
Michal Vanco <michal@maximale.sk>
2021-09-06 10:46:48 +0200

    Supertab for cmp

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -1,19 +1,52 @@
+local check_back_space = function()
+    local col = vim.fn.col '.' - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' ~= nil
+end
+local luasnip = require("luasnip")
+
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
 local cmp = require 'cmp'
 
 cmp.setup {
     completion = { completeopt = 'menu,menuone,noinsert' },
+
     snippet = {
         expand = function(args)
             require("luasnip").lsp_expand(args.body)
         end
     },
+
     mapping = {
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-e>"] = cmp.mapping.close(),
         ["<c-y>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true },
-        ["<C-Space>"] = cmp.mapping.complete()
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if vim.fn.pumvisible() == 1 then
+                vim.fn.feedkeys(t("<C-n>"), "n")
+            elseif luasnip.expand_or_jumpable() then
+                vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"), "")
+            elseif check_back_space() then
+                vim.fn.feedkeys(t("<Tab>"), "n")
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if vim.fn.pumvisible() == 1 then
+                vim.fn.feedkeys(t("<C-p>"), "n")
+            elseif luasnip.jumpable(-1) then
+                vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
+            else
+                fallback()
+            end
+        end, { "i", "s" })
     },
+
     formatting = {
         format = function(entry, vim_item)
             -- fancy icons and a name of kind
@@ -31,6 +64,7 @@ cmp.setup {
             return vim_item
         end
     },
+
     sources = {
         { name = 'buffer' },
         { name = 'path' },
