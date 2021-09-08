802483d1a52f9b6fc591bbd7e815096ffd3322d2
Michal Vanco <michal@maximale.sk>
2021-09-08 07:09:42 +0200

    dapui: silence deprecation warnings

--- lua/mv/dapui.lua
+++ lua/mv/dapui.lua
@@ -17,13 +17,13 @@ require("dapui").setup({
             "breakpoints",
             "watches"
         },
-        width = 40,
+        size = 40,
         position = "left" -- Can be "left" or "right"
     },
     tray = {
         open_on_start = true,
         elements = { "repl" },
-        height = 10,
+        size = 10,
         position = "bottom" -- Can be "bottom" or "top"
     },
     floating = {
