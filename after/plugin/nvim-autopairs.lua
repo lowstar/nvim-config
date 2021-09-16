5d7cef060215367a56ef77a0d338e6a90bbb7490
Michal Vanco <michal@maximale.sk>
2021-09-16 21:19:20 +0200

    nvim-autopairs

--- after/plugin/nvim-autopairs.lua
+++ after/plugin/nvim-autopairs.lua
@@ -1,8 +1,4 @@
-require('nvim-autopairs').setup()
+require('nvim-autopairs').setup({ enable_moveright = false })
 
-require("nvim-autopairs.completion.cmp").setup({
-    map_cr = true, --  map <CR> on insert mode
-    map_complete = true, -- it will auto insert `(` after select function or method item
-    auto_select = true -- automatically select the first item
-})
+require("nvim-autopairs.completion.cmp").setup()
 
