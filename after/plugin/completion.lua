c77a2f074b0010093839a5b43128d958674ea071
Michal Vanco <michal@maximale.sk>
2021-10-28 12:46:30 +0200

    Completion changes

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -1,4 +1,5 @@
 local cmp = require 'cmp'
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
 cmp.setup {
     -- completion = { completeopt = 'menu,menuone,noinsert' },
@@ -10,11 +11,12 @@ cmp.setup {
     },
 
     mapping = {
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<C-y>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true }
+        ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-e>'] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+        ["<C-y>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true },
+        ["<C-q>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true }
     },
 
     formatting = {
@@ -24,19 +26,28 @@ cmp.setup {
         }
     },
 
-    sources = {
+    sources = cmp.config.sources({
         { name = "nvim_lua" },
         { name = "nvim_lsp", priority = 10 },
         { name = "path" },
         { name = "buffer", priority = 2, keyword_length = 5, max_item_count = 5 },
         { name = "luasnip" }
-    },
+    }),
 
-    documentation = { border = 'rounded' }
+    documentation = { border = 'rounded' },
 
+    experimental = { ghost_text = true }
 }
+
+-- Use buffer source for `/`.
+---@diagnostic disable-next-line: undefined-field
+cmp.setup.cmdline('/', { sources = { { name = 'buffer' } } })
+
+-- Use cmdline & path source for ':'.
+---@diagnostic disable-next-line: undefined-field
+cmp.setup.cmdline(':', { sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } }) })
 
 require('nvim-autopairs').setup()
 
-require("nvim-autopairs.completion.cmp").setup { auto_select = false }
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 
--- lua/mv/plugins.lua
+++ lua/mv/plugins.lua
@@ -59,6 +59,7 @@ return require('packer').startup(function(use)
             "hrsh7th/cmp-nvim-lua",
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
             "L3MON4D3/LuaSnip",
             "saadparwaiz1/cmp_luasnip",
             'rafamadriz/friendly-snippets'
