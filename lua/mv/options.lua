5617d0543ebd8cbd539189448885a688cf83883d
Michal Vanco <michal@maximale.sk>
2022-01-03 13:47:49 +0100

    Remember file position

--- lua/mv/options.lua
+++ lua/mv/options.lua
@@ -50,3 +50,7 @@ if vim.fn.executable('rg') then
     vim.o.grepformat = '%f:%l:%c:%m'
 end
 
+vim.cmd [[
+    autocmd BufRead * autocmd FileType <buffer> ++once
+      \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
+]]
