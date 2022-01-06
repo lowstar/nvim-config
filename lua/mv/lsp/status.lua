a4ea2700635d65a894c93fa31288752ab137f8f0
Michal Vanco <michal@maximale.sk>
2022-01-06 21:34:27 +0100

    Reformat with stylua

--- lua/mv/lsp/status.lua
+++ lua/mv/lsp/status.lua
@@ -1,9 +1,9 @@
-local lsp_status = require "lsp-status"
+local lsp_status = require("lsp-status")
 
 local M = {}
 
 M.setup = function()
-    lsp_status.config { indicator_separator = '', component_separator = ' ', status_symbol = '', diagnostics = false }
+    lsp_status.config({ indicator_separator = "", component_separator = " ", status_symbol = "", diagnostics = false })
 
     lsp_status.register_progress()
 end
