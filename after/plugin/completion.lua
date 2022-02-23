2f641393d647142f25c69b80c21e30c21b8bd82b
Michal Vanco <michal@maximale.sk>
2022-02-23 15:18:18 +0100

    Update nvim-cmp config

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -1,15 +1,8 @@
 local cmp = require("cmp")
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
---[[ local luasnip = require("luasnip")
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end ]]
-
+---@diagnostic disable-next-line: redundant-parameter
 cmp.setup({
-    -- completion = { completeopt = 'menu,menuone,noinsert' },
-
     snippet = {
         expand = function(args)
             require("luasnip").lsp_expand(args.body)
@@ -20,35 +13,39 @@ cmp.setup({
         ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
-        ["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
-        ["<C-q>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-
-        --[[ ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-
-        ['<CR>'] = cmp.mapping.confirm({ select = true }) ]]
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.config.disable,
+        ["<S-Tab>"] = cmp.config.disable,
     },
 
+    sorting = {
+        comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+
+            -- copied from cmp-under, but I don't think I need the plugin for this.
+            -- I might add some more of my own.
+            function(entry1, entry2)
+                local _, entry1_under = entry1.completion_item.label:find("^_+")
+                local _, entry2_under = entry2.completion_item.label:find("^_+")
+                entry1_under = entry1_under or 0
+                entry2_under = entry2_under or 0
+                if entry1_under > entry2_under then
+                    return false
+                elseif entry1_under < entry2_under then
+                    return true
+                end
+            end,
+
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+        },
+    },
+
+    -- view = { entries = "native" },
     formatting = {
         format = require("lspkind").cmp_format({
             mode = "symbol_text",
@@ -62,6 +59,7 @@ cmp.setup({
         { name = "nvim_lsp" },
         { name = "path" },
         { name = "luasnip" },
+    }, {
         { name = "buffer" },
     }),
 
@@ -69,16 +67,6 @@ cmp.setup({
 
     experimental = { ghost_text = true },
 })
-
--- Use buffer source for `/`.
----@diagnostic disable-next-line: undefined-field
--- cmp.setup.cmdline('/', { completion = { autocomplete = false }, sources = cmp.config.sources({ { name = 'buffer' } }) })
-
--- Use cmdline & path source for ':'.
----@diagnostic disable-next-line: undefined-field
---[[ cmp.setup.cmdline(':', {
-    sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline', keyword_length = 4 } })
-}) ]]
 
 require("nvim-autopairs").setup({ fast_wrap = {} })
 
