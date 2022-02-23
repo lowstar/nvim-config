63a18450842224be25185980b0fd34179bba2848
Michal Vanco <michal@maximale.sk>
2022-02-23 15:15:10 +0100

    Remove unused plugins

--- lua/mv/plugins.lua
+++ lua/mv/plugins.lua
@@ -81,9 +81,7 @@ return require("packer").startup(function(use)
 
     use({ "folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons" })
 
-    -- use 'airblade/vim-gitgutter'
     use("tpope/vim-fugitive")
-    -- use 'tpope/vim-commentary'
     use("tpope/vim-surround")
     use("machakann/vim-sandwich")
     use("windwp/nvim-autopairs")
